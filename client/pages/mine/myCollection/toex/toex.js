4c1fe1255173030a95b2a2518d895da4051e306d
MyNeighborTotoro <2304943796@qq.com>
2018-05-02 15:16:45 +0800

    ss

--- client/pages/mine/myCollection/toex/toex.js
+++ client/pages/mine/myCollection/toex/toex.js
@@ -14,19 +14,24 @@ Page({
     })
   },
   clickLike: function (event) {
-    var index = event.currentTarget.dataset.index;
-    var key_like = "array[" + index + "].like";
-    var key_button_like = "array[" + index + "].button_like";
-    if (event.currentTarget.dataset.src == "../../../images/like.png") {
-      this.setData({
-        [key_like]: true,
-        [key_button_like]: "../../../images/like1.png"
-      })
-    } else {
-      this.setData({
-        [key_like]: false,
-        [key_button_like]: "../../../images/like.png"
-      })
-    }
+    // var index = event.currentTarget.dataset.index;
+    // var key_like = "array[" + index + "].like";
+    // var key_button_like = "array[" + index + "].button_like";
+    // if (event.currentTarget.dataset.src == "../../../images/like.png") {
+    //   this.setData({
+    //     [key_like]: true,
+    //     [key_button_like]: "../../../images/like1.png"
+    //   })
+    // } else {
+    //   this.setData({
+    //     [key_like]: false,
+    //     [key_button_like]: "../../../images/like.png"
+    //   })
+    // }
+  },
+  onToexTap: function(e){
+    wx.navigateTo({
+      url: '../../../exBook/exBook_detail/exBook_detail',
+    })
   }
 })
