cd77f1b35f5696278ffe52ad1abc342994fcbb7b
MyNeighborTotoro <2304943796@qq.com>
2018-05-03 12:30:22 +0800

    s

--- client/data/books_data.js
+++ client/data/books_data.js
@@ -14,6 +14,8 @@ var local_database = {
       src: "https://wx3.sinaimg.cn/mw690/6c465004gy1fqx992mfidj209z0dw758.jpg"
     }
   ],
+
+  
   book_message_array: [
     {
       bookName: "撒哈拉的故事",
@@ -31,9 +33,7 @@ var local_database = {
       bookIsbn: "9787540455958",
       bookID: "1",
       classify: ""
-},
-
-  {
+  },{
       bookName: "数学之美",
       bookImage: "https://img3.doubanio.com/view/subject/l/public/s9114855.jpg",
       bookAuthor: "吴军",
@@ -41,42 +41,152 @@ var local_database = {
       bookIsbn: "9787115282828",
       bookID: "2",
       classify: ""
-},
+  },
 
-  {
-      bookName: "白夜行",
-      bookImage: "https://img3.doubanio.com/view/subject/l/public/s4610502.jpg",
-      bookAuthor: "东野圭吾",
-      bookSummary: "只希望能手牵手在太阳下散步”，这个象征故事内核的绝望念想，有如一个美丽的幌子，随着无数凌乱、压抑、悲凉的故事片段像纪录片一样一一还原：没有痴痴相思，没有海枯石烂，只剩下一个冰冷绝望的诡计，最后一丝温情也被完全抛弃，万千读者在一曲救赎罪恶的凄苦爱情中悲切动容……",
-      bookIsbn: "9787544242516",
-      bookID: "3",
-      classify: ""
-},
+    {
+        bookName: "白夜行",
+        bookImage: "https://img3.doubanio.com/view/subject/l/public/s4610502.jpg",
+        bookAuthor: "东野圭吾",
+        bookSummary: "只希望能手牵手在太阳下散步”，这个象征故事内核的绝望念想，有如一个美丽的幌子，随着无数凌乱、压抑、悲凉的故事片段像纪录片一样一一还原：没有痴痴相思，没有海枯石烂，只剩下一个冰冷绝望的诡计，最后一丝温情也被完全抛弃，万千读者在一曲救赎罪恶的凄苦爱情中悲切动容……",
+        bookIsbn: "9787544242516",
+        bookID: "3",
+        classify: ""
+  },
 
-  {
-      bookName: "沉默的大多数",
-      bookImage: "https://img1.doubanio.com/view/subject/l/public/s29419068.jpg",
-      bookAuthor: "王小波",
-      bookSummary: "本书收录了王小波的杂文代表作，他以卓越的文采，众醉独醒的姿态对社会道德伦理、国学与新儒家、个体尊严以及小说、艺术等方面进行了酣畅淋漓的剖析，表达了有理有趣的观点。时隔二十年，依然如同清流一般，读来让人沉思，让人捧腹，让人拍案叫绝。",
-      bookIsbn: "9787530216590",
-      bookID: "4",
-      classify: ""
-},
+    {
+        bookName: "沉默的大多数",
+        bookImage: "https://img1.doubanio.com/view/subject/l/public/s29419068.jpg",
+        bookAuthor: "王小波",
+        bookSummary: "本书收录了王小波的杂文代表作，他以卓越的文采，众醉独醒的姿态对社会道德伦理、国学与新儒家、个体尊严以及小说、艺术等方面进行了酣畅淋漓的剖析，表达了有理有趣的观点。时隔二十年，依然如同清流一般，读来让人沉思，让人捧腹，让人拍案叫绝。",
+        bookIsbn: "9787530216590",
+        bookID: "4",
+        classify: ""
+  },
 
-  {
-    bookName: "粉墨",
-    bookImage: "https://img3.doubanio.com/view/subject/l/public/s29731173.jpg",
-    bookAuthor: "张天翼",
-    bookSummary: "《粉墨》收入了张天翼十一篇散文，共三辑：《地下的铁》《出行记》《从透明到灰烬》，讲述了作者的私人生活以及亲人的故事。其中《租房记》描述了作者二十余年的租房史，《从透明到灰烬》讲述了姥姥从家族权威到“透明人”再到化为灰烬的过程，正如作者所言：那些关于亲人和故乡的回忆，是环绕着荆棘的果实，是我的珍宝，也令我痛苦。只有把它们讲出来，敷粉着墨地画在纸上，我才感到我驯服了它们，达成和解，获得平静。",
-    bookIsbn: "9787530217979",
-      bookID: "5",
-      classify: ""
-}
-
+    {
+      bookName: "粉墨",
+      bookImage: "https://img3.doubanio.com/view/subject/l/public/s29731173.jpg",
+      bookAuthor: "张天翼",
+      bookSummary: "《粉墨》收入了张天翼十一篇散文，共三辑：《地下的铁》《出行记》《从透明到灰烬》，讲述了作者的私人生活以及亲人的故事。其中《租房记》描述了作者二十余年的租房史，《从透明到灰烬》讲述了姥姥从家族权威到“透明人”再到化为灰烬的过程，正如作者所言：那些关于亲人和故乡的回忆，是环绕着荆棘的果实，是我的珍宝，也令我痛苦。只有把它们讲出来，敷粉着墨地画在纸上，我才感到我驯服了它们，达成和解，获得平静。",
+      bookIsbn: "9787530217979",
+        bookID: "5",
+        classify: ""
+  }
   ],
   user_array: [
     {
-      
+      userId : "0",
+      userImage: "http://img.zcool.cn/community/016b3756ea1b1a32f875520fc0224b.jpg@900w_1l_2o_100sh.jpg",
+      userGrade: "",
+      userName: "",
+      userCollection: [
+        
+      ],
+      userMessage: {
+
+      }
+    }, {
+      userId: "1",
+      userImage: "http://img63.nipic.com/file/20150416/20852081_222349129000_1.jpg",
+      userGrade: "",
+      userName: "",
+      userCollection: [
+
+      ],
+      userMessage: {
+
+      }
+    }, {
+      userId: "2",
+      userImage: "http://img.zcool.cn/community/016fdd5819a73fa84a0e282be9a3a1.jpg",
+      userGrade: "",
+      userName: "",
+      userCollection: [
+
+      ],
+      userMessage: {
+
+      }
+    }, {
+      userId: "3",
+      userImage: "http://img.zcool.cn/community/01a1b75819a73fa84a0e282bc24651.jpg",
+      userGrade: "",
+      userName: "",
+      userCollection: [
+
+      ],
+      userMessage: {
+
+      }
+    }, {
+      userId: "4",
+      userImage: "http://img0w.pconline.com.cn/pconline/1403/29/spcgroup/width_640,,qua_30/4523636_04.jpg",
+      userGrade: "",
+      userName: "",
+      userCollection: [
+
+      ],
+      userMessage: {
+
+      }
+    }, {
+      userId: "5",
+      userImage: "http://www.touxiang.cn/uploads/20131012/12-054249_479.jpg",
+      userGrade: "",
+      userName: "",
+      userCollection: [
+
+      ],
+      userMessage: {
+
+      }
+    }, {
+      userId: "6",
+      userImage: "http://www.qqzhi.com/uploadpic/2015-01-14/204348907.jpg",
+      userGrade: "",
+      userName: "",
+      userCollection: [
+
+      ],
+      userMessage: {
+
+      }
+    }, {
+      userId: "7",
+      userImage: "http://pic.58pic.com/58pic/16/83/97/18258PIC9Jv_1024.jpg",
+      userGrade: "",
+      userName: "",
+      userCollection: [
+
+      ],
+      userMessage: {
+
+      }
+    }, {
+      userId: "8",
+      userImage: "http://img03.tooopen.com/uploadfile/Downs/images/2012/2/24/sy_20120224133241944132.jpg",
+      userGrade: "",
+      userName: "",
+      userCollection: [
+
+      ],
+      userMessage: {
+
+      }
+    }
+  ],
+  havedBook_message_array: [
+    {
+      bookName: "偷影子的人",
+      bookImage: "https://img1.doubanio.com/view/subject/l/public/s10339418.jpg",
+      bookAuthor: "马克•李维",
+      bookSummary: "《偷影子的人》是马克•李维的第10部作品，该书在法国首印45万册，位列全年销量排行榜的总冠军，法国媒体惊叹：“销售得比影子消失的速度还快！”这部作品完美展现了马克•李维温柔风趣的写作风格，有催人泪下的亲情、浪漫感人的爱情和不离不弃的友情，清新浪漫的气息和温柔感人的故事相互交织，带给读者笑中带泪的阅读感受，是一部唤醒童年回忆和内心梦想的温情疗愈小说。",
+      bookIsbn: "9787540455958",
+      bookID: "1",
+      classify: "",
+      userId: [
+
+      ]
     }
   ]
 }
--- client/pages/homePage/homePage.js
+++ client/pages/homePage/homePage.js
@@ -10,38 +10,10 @@ Page({
     swiper_type: true,
     bottom_border_left: "bottom_border_left",
     bottom_border_right: "bottom_border_right",
-    rank_list_array : [
-      {
-        rankImage: "http://img.zcool.cn/community/016b3756ea1b1a32f875520fc0224b.jpg@900w_1l_2o_100sh.jpg",
-        Lv: 24
-      },{
-        rankImage: "http://img63.nipic.com/file/20150416/20852081_222349129000_1.jpg",
-        Lv: 24
-      },{
-        rankImage: "http://img.zcool.cn/community/016fdd5819a73fa84a0e282be9a3a1.jpg",
-        Lv: 24
-      },{
-        rankImage: "http://img.zcool.cn/community/01a1b75819a73fa84a0e282bc24651.jpg",
-        Lv: 24
-      },{
-        rankImage: "http://img0w.pconline.com.cn/pconline/1403/29/spcgroup/width_640,,qua_30/4523636_04.jpg",
-        Lv: 24
-      },{
-        rankImage: "http://www.touxiang.cn/uploads/20131012/12-054249_479.jpg",
-        Lv: 24
-      },{
-        rankImage: "http://www.qqzhi.com/uploadpic/2015-01-14/204348907.jpg",
-        Lv: 24
-      },{   
-        rankImage: "http://pic.58pic.com/58pic/16/83/97/18258PIC9Jv_1024.jpg",
-        Lv: 24
-      },{
-        rankImage: "http://img03.tooopen.com/uploadfile/Downs/images/2012/2/24/sy_20120224133241944132.jpg",
-        Lv: 24
-      }
-    ],
-    array : postdata.postlist.book_message_array,
-    back_array: postdata.postlist.back_array
+    
+    book_message_array : "",
+    back_array: "" ,
+    user_array: ""
   },
   toOthers_detail:function(e){
     wx.navigateTo({
@@ -94,6 +66,14 @@ Page({
    * 生命周期函数--监听页面加载
    */
   
+  onLoad: function(e){
+    this.setData({
+      book_message_array: postdata.postlist.book_message_array,
+      back_array: postdata.postlist.back_array,
+      user_array: postdata.postlist.user_array,
+      havedBook_message_array: postdata.postlist.havedBook_message_array
+    })
+  },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
