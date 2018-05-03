af019c704e535482e610ca288f2f971534e3ab4d
MyNeighborTotoro <2304943796@qq.com>
2018-05-03 18:02:03 +0800

    aadss

--- client/pages/homePage/homePage_swiper_detail/homePage_swiper_detail.js
+++ client/pages/homePage/homePage_swiper_detail/homePage_swiper_detail.js
@@ -10,7 +10,7 @@ Page({
     onmycollection: false,
     bookname: "",
     bookimage: "",
-    bookauthor: "", 
+    bookauthor: "",
     comments: [{
       title: "沉默的话语权",
       content: "小程序组件 scroll-view 中分别有上下竖向滑动和左右横向滑动之分，在这次项目中刚好需要用到横向滑动，但在测试过程中发现横向滑动没有了效果（静止在那里没移动过），经调试发现1.scroll-view 中的需要滑动的元素不可以用float 浮动2.scroll-view 中的包裹需要滑动的元素的大盒子用 display:flex; 是没有作用的",
@@ -23,21 +23,21 @@ Page({
       name: "Author",
       likecount: "240",
       commentcount: "360"
-      }, {
-        title: "沉默的话语权",
-        content: "小程序组件 scroll-view 中分别有上下竖向滑动和左右横向滑动之分，在这次项目中刚好需要用到横向滑动，但在测试过程中发现横向滑动没有了效果（静止在那里没移动过），经调试发现1.scroll-view 中的需要滑动的元素不可以用float 浮动2.scroll-view 中的包裹需要滑动的元素的大盒子用 display:flex; 是没有作用的",
-        name: "Author",
-        likecount: "240",
-        commentcount: "360"
-      }
+    }, {
+      title: "沉默的话语权",
+      content: "小程序组件 scroll-view 中分别有上下竖向滑动和左右横向滑动之分，在这次项目中刚好需要用到横向滑动，但在测试过程中发现横向滑动没有了效果（静止在那里没移动过），经调试发现1.scroll-view 中的需要滑动的元素不可以用float 浮动2.scroll-view 中的包裹需要滑动的元素的大盒子用 display:flex; 是没有作用的",
+      name: "Author",
+      likecount: "240",
+      commentcount: "360"
+    }
     ]
   },
-  onAvatorTap: function(e){
+  onAvatorTap: function (e) {
     wx.navigateTo({
       url: '../../mine/myCollection/toex/toex',
     })
   },
-  onLoad: function(option){
+  onLoad: function (option) {
     this.setData({
       bookid: option.bookid,
       bookname: option.bookname,
@@ -47,22 +47,22 @@ Page({
       my_: app.globalData.my_
     })
   },
-  clickLike: function(e){
-    
-    if(this.data.onmycollection == 'true'){
-      
-      for (var i = 0; i < this.data.my_.userCollection.length;i++){
-        if (this.data.bookid == this.data.my_.userCollection[i]){
-          this.data.my_.userCollection.splice(i,1);
+  clickLike: function (e) {
+
+    if (this.data.onmycollection == 'true') {
+
+      for (var i = 0; i < this.data.my_.userCollection.length; i++) {
+        if (this.data.bookid == this.data.my_.userCollection[i]) {
+          this.data.my_.userCollection.splice(i, 1);
           this.setData({
             my_: this.data.my_,
             onmycollection: 'false'
           });
           // app.globalData.my_ = this.data.my_;
-          
+
         }
       }
-    }else{
+    } else {
       this.data.my_.userCollection.push(this.data.bookid);
       this.setData({
         my_: this.data.my_,
@@ -71,7 +71,7 @@ Page({
       // app.globalData.my_ = this.data.my_;
     }
   },
-  matchOnmycollection:function(e){
-    
+  matchOnmycollection: function (e) {
+
   }
 })
