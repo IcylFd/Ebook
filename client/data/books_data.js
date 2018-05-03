f03324155f489c4987beed4186792847fd33ac3e
MyNeighborTotoro <2304943796@qq.com>
2018-05-03 15:10:39 +0800

    s

--- client/data/books_data.js
+++ client/data/books_data.js
@@ -15,63 +15,69 @@ var local_database = {
     }
   ],
 
-  
+
   book_message_array: [
-    {
-      bookName: "撒哈拉的故事",
-      bookImage: "https://img3.doubanio.com/view/subject/l/public/s6780941.jpg",
-      bookAuthor: "三毛",
-      bookSummary:"三毛作品中最脍炙人口当属《撒哈拉的故事》，本书由十几篇精彩动人的散文结合而成，其中《沙漠中的饭店》，是三毛适应荒凉单调的沙漠生活后，重新拾笔的第一篇文章，从此之后，三毛便写出一系列以沙漠为背景的故事，倾倒了全世界的华文读者。",
-      bookIsbn:"9787540455958",
-      bookID:"0",
-      classify: ""
-  },{
-      bookName: "偷影子的人",
-      bookImage: "https://img1.doubanio.com/view/subject/l/public/s10339418.jpg",
-      bookAuthor:"马克•李维",
-      bookSummary: "《偷影子的人》是马克•李维的第10部作品，该书在法国首印45万册，位列全年销量排行榜的总冠军，法国媒体惊叹：“销售得比影子消失的速度还快！”这部作品完美展现了马克•李维温柔风趣的写作风格，有催人泪下的亲情、浪漫感人的爱情和不离不弃的友情，清新浪漫的气息和温柔感人的故事相互交织，带给读者笑中带泪的阅读感受，是一部唤醒童年回忆和内心梦想的温情疗愈小说。",
-      bookIsbn: "9787540455958",
-      bookID: "1",
-      classify: ""
-  },{
-      bookName: "数学之美",
-      bookImage: "https://img3.doubanio.com/view/subject/l/public/s9114855.jpg",
-      bookAuthor: "吴军",
-      bookSummary: "作者吴军博士几乎把所有文章都重写了一遍，为的是把高深的数学原理讲得更加通俗易懂，让非专业读者也能领略数学的魅力。读者通过具体的例子学到的是思考问题的方式 —— 如何化繁为简，如何用数学去解决工程问题，如何跳出固有思维不断去思考创新。",
-      bookIsbn: "9787115282828",
-      bookID: "2",
-      classify: ""
-  },
+        {
+          bookName: "撒哈拉的故事",
+          bookImage: "https://img3.doubanio.com/view/subject/l/public/s6780941.jpg",
+          bookAuthor: "三毛",
+          bookSummary:"三毛作品中最脍炙人口当属《撒哈拉的故事》，本书由十几篇精彩动人的散文结合而成，其中《沙漠中的饭店》，是三毛适应荒凉单调的沙漠生活后，重新拾笔的第一篇文章，从此之后，三毛便写出一系列以沙漠为背景的故事，倾倒了全世界的华文读者。",
+          bookIsbn:"9787540455958",
+          bookID:"0",
+          classify: "",
+          onMyCollection: false
+      },{
+          bookName: "偷影子的人",
+          bookImage: "https://img1.doubanio.com/view/subject/l/public/s10339418.jpg",
+          bookAuthor:"马克•李维",
+          bookSummary: "《偷影子的人》是马克•李维的第10部作品，该书在法国首印45万册，位列全年销量排行榜的总冠军，法国媒体惊叹：“销售得比影子消失的速度还快！”这部作品完美展现了马克•李维温柔风趣的写作风格，有催人泪下的亲情、浪漫感人的爱情和不离不弃的友情，清新浪漫的气息和温柔感人的故事相互交织，带给读者笑中带泪的阅读感受，是一部唤醒童年回忆和内心梦想的温情疗愈小说。",
+          bookIsbn: "9787540455958",
+          bookID: "1",
+          classify: "",
+          onMyCollection: false
+      },{
+          bookName: "数学之美",
+          bookImage: "https://img3.doubanio.com/view/subject/l/public/s9114855.jpg",
+          bookAuthor: "吴军",
+          bookSummary: "作者吴军博士几乎把所有文章都重写了一遍，为的是把高深的数学原理讲得更加通俗易懂，让非专业读者也能领略数学的魅力。读者通过具体的例子学到的是思考问题的方式 —— 如何化繁为简，如何用数学去解决工程问题，如何跳出固有思维不断去思考创新。",
+          bookIsbn: "9787115282828",
+          bookID: "2",
+          classify: "",
+          onMyCollection: false
+      },
 
-    {
-        bookName: "白夜行",
-        bookImage: "https://img3.doubanio.com/view/subject/l/public/s4610502.jpg",
-        bookAuthor: "东野圭吾",
-        bookSummary: "只希望能手牵手在太阳下散步”，这个象征故事内核的绝望念想，有如一个美丽的幌子，随着无数凌乱、压抑、悲凉的故事片段像纪录片一样一一还原：没有痴痴相思，没有海枯石烂，只剩下一个冰冷绝望的诡计，最后一丝温情也被完全抛弃，万千读者在一曲救赎罪恶的凄苦爱情中悲切动容……",
-        bookIsbn: "9787544242516",
-        bookID: "3",
-        classify: ""
-  },
+        {
+            bookName: "白夜行",
+            bookImage: "https://img3.doubanio.com/view/subject/l/public/s4610502.jpg",
+            bookAuthor: "东野圭吾",
+            bookSummary: "只希望能手牵手在太阳下散步”，这个象征故事内核的绝望念想，有如一个美丽的幌子，随着无数凌乱、压抑、悲凉的故事片段像纪录片一样一一还原：没有痴痴相思，没有海枯石烂，只剩下一个冰冷绝望的诡计，最后一丝温情也被完全抛弃，万千读者在一曲救赎罪恶的凄苦爱情中悲切动容……",
+            bookIsbn: "9787544242516",
+            bookID: "3",
+            classify: "",
+            onMyCollection: false
+      },
 
-    {
-        bookName: "沉默的大多数",
-        bookImage: "https://img1.doubanio.com/view/subject/l/public/s29419068.jpg",
-        bookAuthor: "王小波",
-        bookSummary: "本书收录了王小波的杂文代表作，他以卓越的文采，众醉独醒的姿态对社会道德伦理、国学与新儒家、个体尊严以及小说、艺术等方面进行了酣畅淋漓的剖析，表达了有理有趣的观点。时隔二十年，依然如同清流一般，读来让人沉思，让人捧腹，让人拍案叫绝。",
-        bookIsbn: "9787530216590",
-        bookID: "4",
-        classify: ""
-  },
+        {
+            bookName: "沉默的大多数",
+            bookImage: "https://img1.doubanio.com/view/subject/l/public/s29419068.jpg",
+            bookAuthor: "王小波",
+            bookSummary: "本书收录了王小波的杂文代表作，他以卓越的文采，众醉独醒的姿态对社会道德伦理、国学与新儒家、个体尊严以及小说、艺术等方面进行了酣畅淋漓的剖析，表达了有理有趣的观点。时隔二十年，依然如同清流一般，读来让人沉思，让人捧腹，让人拍案叫绝。",
+            bookIsbn: "9787530216590",
+            bookID: "4",
+            classify: "",
+            onMyCollection: false
+      },
 
-    {
-      bookName: "粉墨",
-      bookImage: "https://img3.doubanio.com/view/subject/l/public/s29731173.jpg",
-      bookAuthor: "张天翼",
-      bookSummary: "《粉墨》收入了张天翼十一篇散文，共三辑：《地下的铁》《出行记》《从透明到灰烬》，讲述了作者的私人生活以及亲人的故事。其中《租房记》描述了作者二十余年的租房史，《从透明到灰烬》讲述了姥姥从家族权威到“透明人”再到化为灰烬的过程，正如作者所言：那些关于亲人和故乡的回忆，是环绕着荆棘的果实，是我的珍宝，也令我痛苦。只有把它们讲出来，敷粉着墨地画在纸上，我才感到我驯服了它们，达成和解，获得平静。",
-      bookIsbn: "9787530217979",
-        bookID: "5",
-        classify: ""
-  }
+        {
+          bookName: "粉墨",
+          bookImage: "https://img3.doubanio.com/view/subject/l/public/s29731173.jpg",
+          bookAuthor: "张天翼",
+          bookSummary: "《粉墨》收入了张天翼十一篇散文，共三辑：《地下的铁》《出行记》《从透明到灰烬》，讲述了作者的私人生活以及亲人的故事。其中《租房记》描述了作者二十余年的租房史，《从透明到灰烬》讲述了姥姥从家族权威到“透明人”再到化为灰烬的过程，正如作者所言：那些关于亲人和故乡的回忆，是环绕着荆棘的果实，是我的珍宝，也令我痛苦。只有把它们讲出来，敷粉着墨地画在纸上，我才感到我驯服了它们，达成和解，获得平静。",
+          bookIsbn: "9787530217979",
+            bookID: "5",
+            classify: "",
+            onMyCollection: false
+      }
   ],
   user_array: [
     {
@@ -182,13 +188,79 @@ var local_database = {
       bookAuthor: "马克•李维",
       bookSummary: "《偷影子的人》是马克•李维的第10部作品，该书在法国首印45万册，位列全年销量排行榜的总冠军，法国媒体惊叹：“销售得比影子消失的速度还快！”这部作品完美展现了马克•李维温柔风趣的写作风格，有催人泪下的亲情、浪漫感人的爱情和不离不弃的友情，清新浪漫的气息和温柔感人的故事相互交织，带给读者笑中带泪的阅读感受，是一部唤醒童年回忆和内心梦想的温情疗愈小说。",
       bookIsbn: "9787540455958",
-      bookID: "1",
+      bookID: "6",
       classify: "",
-      userId: [
-
-      ]
+      userId: [],
+      onMyCollection: false
+    },
+    {
+      bookName: "偷影子的人",
+      bookImage: "https://img1.doubanio.com/view/subject/l/public/s10339418.jpg",
+      bookAuthor: "马克•李维",
+      bookSummary: "《偷影子的人》是马克•李维的第10部作品，该书在法国首印45万册，位列全年销量排行榜的总冠军，法国媒体惊叹：“销售得比影子消失的速度还快！”这部作品完美展现了马克•李维温柔风趣的写作风格，有催人泪下的亲情、浪漫感人的爱情和不离不弃的友情，清新浪漫的气息和温柔感人的故事相互交织，带给读者笑中带泪的阅读感受，是一部唤醒童年回忆和内心梦想的温情疗愈小说。",
+      bookIsbn: "9787540455958",
+      bookID: "7",
+      classify: "",
+      userId: [],
+      onMyCollection: false
+    }, {
+      bookName: "偷影子的人",
+      bookImage: "https://img1.doubanio.com/view/subject/l/public/s10339418.jpg",
+      bookAuthor: "马克•李维",
+      bookSummary: "《偷影子的人》是马克•李维的第10部作品，该书在法国首印45万册，位列全年销量排行榜的总冠军，法国媒体惊叹：“销售得比影子消失的速度还快！”这部作品完美展现了马克•李维温柔风趣的写作风格，有催人泪下的亲情、浪漫感人的爱情和不离不弃的友情，清新浪漫的气息和温柔感人的故事相互交织，带给读者笑中带泪的阅读感受，是一部唤醒童年回忆和内心梦想的温情疗愈小说。",
+      bookIsbn: "9787540455958",
+      bookID: "8",
+      classify: "",
+      userId: [],
+      onMyCollection: false
+    }, {
+      bookName: "偷影子的人",
+      bookImage: "https://img1.doubanio.com/view/subject/l/public/s10339418.jpg",
+      bookAuthor: "马克•李维",
+      bookSummary: "《偷影子的人》是马克•李维的第10部作品，该书在法国首印45万册，位列全年销量排行榜的总冠军，法国媒体惊叹：“销售得比影子消失的速度还快！”这部作品完美展现了马克•李维温柔风趣的写作风格，有催人泪下的亲情、浪漫感人的爱情和不离不弃的友情，清新浪漫的气息和温柔感人的故事相互交织，带给读者笑中带泪的阅读感受，是一部唤醒童年回忆和内心梦想的温情疗愈小说。",
+      bookIsbn: "9787540455958",
+      bookID: "9",
+      classify: "",
+      userId: [],
+      onMyCollection: false
+    }, {
+      bookName: "偷影子的人",
+      bookImage: "https://img1.doubanio.com/view/subject/l/public/s10339418.jpg",
+      bookAuthor: "马克•李维",
+      bookSummary: "《偷影子的人》是马克•李维的第10部作品，该书在法国首印45万册，位列全年销量排行榜的总冠军，法国媒体惊叹：“销售得比影子消失的速度还快！”这部作品完美展现了马克•李维温柔风趣的写作风格，有催人泪下的亲情、浪漫感人的爱情和不离不弃的友情，清新浪漫的气息和温柔感人的故事相互交织，带给读者笑中带泪的阅读感受，是一部唤醒童年回忆和内心梦想的温情疗愈小说。",
+      bookIsbn: "9787540455958",
+      bookID: "10",
+      classify: "",
+      userId: [],
+      onMyCollection: false
+    }, {
+      bookName: "偷影子的人",
+      bookImage: "https://img1.doubanio.com/view/subject/l/public/s10339418.jpg",
+      bookAuthor: "马克•李维",
+      bookSummary: "《偷影子的人》是马克•李维的第10部作品，该书在法国首印45万册，位列全年销量排行榜的总冠军，法国媒体惊叹：“销售得比影子消失的速度还快！”这部作品完美展现了马克•李维温柔风趣的写作风格，有催人泪下的亲情、浪漫感人的爱情和不离不弃的友情，清新浪漫的气息和温柔感人的故事相互交织，带给读者笑中带泪的阅读感受，是一部唤醒童年回忆和内心梦想的温情疗愈小说。",
+      bookIsbn: "9787540455958",
+      bookID: "11",
+      classify: "",
+      userId: [],
+      onMyCollection: false
     }
-  ]
+  ],
+  ExchangeRecord_array: [
+
+  ],
+
+
+
+  my_: {
+    userId: "0",
+    userImage: "http://img.zcool.cn/community/016b3756ea1b1a32f875520fc0224b.jpg@900w_1l_2o_100sh.jpg",
+    userGrade: "",
+    userName: "",
+    userCollection: ["0","3","8"],
+    userMessage: {
+
+    }
+  }
 }
 
 module.exports = {
